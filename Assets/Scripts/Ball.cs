2fdccce55e11e795065f96937285b08417bdf3dc
NOTEOXI <eiden96004@gmail.com>
2023-02-04 16:49:39 +0900

    ver0.93 α 各処理の説明のためのコメントを追加

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -28,8 +28,12 @@ public class Ball : MonoBehaviour
     {
         collisionHandler = GetComponent<CollisionHandler>();
 
+        //当たり判定のプログラムにボールが
+        //オブジェクトに当たったら反射するプログラムを設定
         collisionHandler.collisionEnterEvent += ReflectBall;
+        //ゲームオブジェクトから物理計算プログラムを取得
         rb = GetComponent<Rigidbody>();
+        //サウンドのプログラムを保存
         this.sound = sound;
     }
 
@@ -38,17 +42,23 @@ public class Ball : MonoBehaviour
     /// </summary>
     public void Move()
     {
-        const int notDir = 0;
-        const int minDir = -1;
-        const int maxDir = 2;
+        const int notDir = 0;   //方向なし
+        const int minDir = -1;  //小さい値
+        const int maxDir = 2;   //大きい値
 
+        //Moveだけ使えるstartDir変数を初期化
         Vector3 startDir = Vector3.zero;
+
+        //xが方向なしの時 または zが方向なしの時
+        //もう一度ランダムに方向を決定する
         while (startDir.x == notDir || startDir.z == notDir)
         {
+            //ランダムに方向を決定する
             startDir.x = UnityEngine.Random.Range(minDir, maxDir);
             startDir.z = UnityEngine.Random.Range(minDir, maxDir);
         }
 
+        //ランダムな方向にスピードを加えて飛ばす
         rb.AddForce(startDir * startSpeed, ForceMode.Impulse);
     }
 
@@ -58,7 +68,9 @@ public class Ball : MonoBehaviour
     /// </summary>
     public void StopMove()
     {
+        //ボールの速度を0に
         rb.velocity = Vector3.zero;
+        //現在のボールの向きをリセット
         ballDir = Vector3.zero;
     }
 
@@ -68,6 +80,7 @@ public class Ball : MonoBehaviour
     /// </summary>
     private void Update()
     {
+        //現在のボールの向きを常に取得
         ballDir = rb.velocity;
     }
 
@@ -79,40 +92,58 @@ public class Ball : MonoBehaviour
     /// <param name="collision"></param>
     void ReflectBall(Collision collision)
     {
+        //現在の向きを反射させる
         Vector3 result = Vector3.Reflect(ballDir, collision.contacts[0].normal);
 
-        //==========================
+        /*==========================
         //上級者向け
         //==========================
+        //当たったオブジェクトがプレイヤーかどうか
         if (collision.transform.tag == playerTag)
         {
+        //プレイヤーの物理演算プログラムを取得
             var playerRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+            //プレイヤーがどっちに移動しているか取得
             var playerMoveSpeed = playerRigidBody.velocity.normalized.z;
-
+            //ボールを反転させるかチェック
             if (PlayerUpMoveCheck(playerMoveSpeed, result.z) ||
                 PlayerDownMoveCheck(playerMoveSpeed, result.z))
             {
+                //飛んできた方向に反転
                 result.z = -result.z;
             }
         }
-        //==========================
+        //==========================*/
 
-        rb.velocity = result;
+        //反射する際の音を再生
         sound.PlaySE(SEType.Reflect);
+        //速度を反映
+        rb.velocity = result;
         ballDir = rb.velocity;
     }
 
-    //==========================
+    /*//==========================
     //上級者向け
     //==========================
-
+    /// <summary>
+    /// プレイヤーが上に移動しているかつボールが下に移動している場合
+    /// </summary>
+    /// <param name="player_speed"></param>
+    /// <param name="ball_dir"></param>
+    /// <returns></returns>
     bool PlayerUpMoveCheck(float player_speed, float ball_dir)
     {
         return player_speed > 0 && ball_dir < 0;
     }
 
+    /// <summary>
+    /// プレイヤーがに下移動しているかつボールが上に移動している場合
+    /// </summary>
+    /// <param name="player_speed"></param>
+    /// <param name="ball_dir"></param>
+    /// <returns></returns>
     bool PlayerDownMoveCheck(float player_speed, float ball_dir)
     {
         return player_speed < 0 && 0 < ball_dir;
-    }
+    }*/
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,7 @@ public class Player : MonoBehaviour
     /// </summary>
     public void Init()
     {
+        //ゲームオブジェクトから物理計算プログラムを取得
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,6 +24,7 @@ public class Player : MonoBehaviour
     /// <param name="value"></param>
     public void InputKey(float value)
     {
+        //外部からの入力をプログラム内に一時保存
         inputKeyValue = value;
     }
 
@@ -32,7 +34,9 @@ public class Player : MonoBehaviour
     /// </summary>
     public void StopMove()
     {
+        //プレイヤーの速度を0に
         rb.velocity = Vector3.zero;
+        //一時保存されている入力データを0に
         inputKeyValue = 0.0f;
     }
 
@@ -42,6 +46,8 @@ public class Player : MonoBehaviour
     /// </summary>
     private void Update()
     {
+        //物理演算の速度に
+        //プレイヤーの上下の動きを固定 * キーの上下入力 * スピード を反映
         rb.velocity = Vector3.forward * inputKeyValue * speed;
     }
 }
